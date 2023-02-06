886f13c63a1c57f84c983384e10e3146607d91f0
Akash <aakash@hey.com>
2023-02-07 00:40:56 +0530

    add breakpoint composables

---
+++ composables/screen.ts
@@ -0,0 +1,8 @@
+import { breakpointsTailwind } from "@vueuse/core"
+
+export const breakpoints = useBreakpoints(breakpointsTailwind)
+
+export const isMobileScreen = breakpoints.smallerOrEqual("sm")
+export const isSmallScreen = breakpoints.smallerOrEqual("md")
+export const isMediumScreen = breakpoints.smallerOrEqual("lg")
+export const isMediumOrLargeScreen = breakpoints.between("sm", "xl")
